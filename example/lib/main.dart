1ccd7d91259fd182ee2b90b05764a971a9d1bfac
Ufuk <ufukzimmerman@gmail.com>
2021-05-07 11:36:57 +0300

    no message

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:safe_device/safe_device.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,40 +15,6 @@ class _MyAppState extends State<MyApp> {
   bool isRealDevice = true;
   bool isOnExternalStorage = false;
   bool isSafeDevice = false;
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-//    String platformVersion;
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    // Platform messages may fail, so we use a try/catch PlatformException.ßß
-    try {
-      isJailBroken = await SafeDevice.isJailBroken;
-      canMockLocation = await SafeDevice.canMockLocation;
-      isRealDevice = await SafeDevice.isRealDevice;
-      isOnExternalStorage = await SafeDevice.isOnExternalStorage;
-      isSafeDevice = await SafeDevice.isSafeDevice;
-    } catch (error) {
-      print(error);
-    }
-
-    setState(() {
-      isJailBroken = isJailBroken;
-      canMockLocation = canMockLocation;
-      isRealDevice = isRealDevice;
-      isOnExternalStorage = isOnExternalStorage;
-      isSafeDevice = isSafeDevice;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
