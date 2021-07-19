14506469a93ca7ed189ba01280b99e5c5fe19034
Hartmut Obendorf <hartmut.obendorf@Chainsteps-MBP.fritz.box>
2021-07-19 18:01:58 +0200

    added two heuristics for Android emulator started with Android Studio

--- android/src/main/java/com/xamdesign/safe_device/Emulator/EmulatorCheck.java
+++ android/src/main/java/com/xamdesign/safe_device/Emulator/EmulatorCheck.java
@@ -7,9 +7,11 @@ public class EmulatorCheck {
         return Build.FINGERPRINT.startsWith("generic")
                 || Build.FINGERPRINT.startsWith("unknown")
                 || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.startsWith("sdk_")
                 || Build.MODEL.contains("Emulator")
                 || Build.MODEL.contains("Android SDK built for x86")
                 || Build.MANUFACTURER.contains("Genymotion")
+                || Build.DEVICE.startsWith("emulator")
                 || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
                 || "google_sdk".equals(Build.PRODUCT);
     }
